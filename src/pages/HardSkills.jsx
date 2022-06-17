ea8b1d53927a1faf32dce026650be6127a5eec95
Fermisson-Gomes <fermison.barbosa@gmail.com>
2022-06-17 17:59:29 -0300

    atualização

--- src/pages/HardSkills.jsx
+++ src/pages/HardSkills.jsx
@@ -1,33 +1,53 @@
 import React from 'react';
 import '../CSS/hardskills.css'
-import { FaNodeJs, FaReact } from 'react-icons/fa';
-import { SiCss3, SiHtml5, SiJavascript } from 'react-icons/si';
+import { FaBootstrap, FaNodeJs, FaReact } from 'react-icons/fa';
+import { SiCss3, SiHtml5, SiJavascript, SiJest, SiRedux } from 'react-icons/si';
 import Card from '../componentes/Card';
 import Back from '../componentes/Back';
 
 const HardSkills = () => {
-  const icons = [
-    <FaReact />,
-    <SiJavascript />,
-    <SiHtml5 />,
-    <SiCss3 />,
-    <FaNodeJs />
+  const techLIst = [
+    {
+      icon: <SiJavascript />,
+      title: 'Javascript',
+    },
+    {
+      icon: <SiHtml5 />,
+      title: 'HTML',
+    },
+    {
+      icon: <SiCss3 />,
+      title: 'CSS',
+    },
+    {
+      icon: <FaNodeJs />,
+      title: 'Node',
+    },
+    {
+      icon: <FaReact />,
+      title: 'React',
+    },
+    {
+      icon: <SiRedux />,
+      title: 'Redux',
+    },
+    {
+      icon: <SiJest />,
+      title: 'Jest',
+    },
+    {
+      icon: <FaBootstrap />,
+      title: 'Bootstrap',
+    },
   ];
-  const titles = [
-    'React',
-    'Javascript',
-    'HTML',
-    'CSS',
-    'Node Js',
-  ]
   return (
     <main className='main'>
       <section className='hardskills'>
         <div className='hardskills-cards'>
         {
-          icons.map((icon, index) => (
+          techLIst.map(({icon, title}, index) => (
             <div key={index} className='card'>
-              <Card icon={ icon } title={ titles[index] } />
+              <Card icon={ icon } title={ title } />
             </div>
           ))
         }
